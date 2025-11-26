0bbc82b50552b213b5b6691dbea32d97ef610697
Kyle Gerner <kgcoltsfan@gmail.com>
2025-11-26 14:33:36 -0500

    update calendar styling to not be cut off for large months

--- components/AvailabilityCalendar.tsx
+++ components/AvailabilityCalendar.tsx
@@ -22,20 +22,18 @@ function CalendarEmbed() {
   // Mobile vs desktop months
   const numMonthsVisible = isMobile ? 1 : 3;
 
-  // CSS URL with cache-busting
-  const cssUrl = `https://bledowski-website.vercel.app/calendar-custom.css?t=${Date.now()}`;
-//   const cssUrl = 
-//     process.env.NODE_ENV === "development"
-//       ? `http://localhost:3000/calendar-custom.css?t=${Date.now()}`
-//       : `https://bledowski-website.vercel.app/calendar-custom.css?t=${Date.now()}`;
+  const cssUrl =
+    process.env.NODE_ENV === "development"
+      ? `http://localhost:3000/calendar-custom.css?t=${Date.now()}`
+      : `https://bledowski-website.vercel.app/calendar-custom.css`;
 
   return (
-    <div className="container mx-auto px-4 my-12">
+    <div className="container mx-auto px-4 my-4">
       <iframe
         src={`https://www.availabilitycalendar.com/embed/uPz5F5VSUXDIKVLeNqcb/en-0-2-${numMonthsVisible}-1-0-0-0-0-0-0-0-1-0/?iframe=true&css=${cssUrl}`}
         style={{
           width: "100%",
-          height: "360px",
+          height: "420px",
           border: "none",
           borderRadius: "16px",
           overflow: "hidden",
@@ -48,7 +46,7 @@ function CalendarEmbed() {
 
 export default function AvailabilityCalendar() {
   return (
-    <div className="bg-white p-8 rounded-lg shadow-lg">
+    <div className="bg-white p-4 rounded-lg shadow-lg">
       <div className="flex flex-col md:items-center md:justify-between gap-4 mb-8">
         <h2 className="text-3xl font-semibold text-gray-800 text-center md:text-left">
           Availability Calendar
