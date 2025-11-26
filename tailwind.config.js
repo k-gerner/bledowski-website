dc6dfe8d9cce31daee59331c46cfbc39b2aacb47
Konrad Bledowski <32736495+kbledows@users.noreply.github.com>
2025-11-26 12:31:58 -0500

    update tsconfig and tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
+    "./app/**/*.{ts,tsx}",
+    "./components/**/*.{ts,tsx}",
   ],
   theme: {
     extend: {
